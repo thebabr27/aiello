b77dcbb71d9e5572c712e53ab7fe0a47792d9739
thebabr27 <47173624+thebabr27@users.noreply.github.com>
2019-03-27 00:29:03 +0100

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -87,43 +87,147 @@ document.getElementById("giorno").innerHTML = day;
 
 var exp = new Date().getHours();
 var exp2 = new Date().getMinutes();
-var exp2def = exp2.toPrecision(2);
-var orario = exp + ":" + exp2def;
-
-document.getElementById("orario").innerHTML = orario;
 
 var bgalba = "linear-gradient(180deg, #9FC1F7 0%, #E48C75 100%)"
 var bgmatt = "linear-gradient(180deg, #3C8AFC 0%, #ACD9F4 100%)"
 var bgpome = "linear-gradient(180deg, #1A6DE1 0%, #8AD4F1 100%)"
 var bgsera = "linear-gradient(180deg, #03223F 0%, #7D4D7B 50.83%, #DD7962 100%)"
 var bgnotte = "linear-gradient(180deg, #000203 0%, #002746 100%)"
-switch (exp) {
 
-    case (0), (1), (2), (3), (4), (5):
-    document.getElementById("allprova").style.background = bgnotte; //h0
-    document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte...";
-    document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani";
-    document.getElementById("rondini1").classList.add = "hidden";
-    break;
-    case (6), (7), (8):
-    document.getElementById("allprova").style.background = bgalba; //h6
-    break;
-    case (9), (10), (11), (12), (13):
-    document.getElementById("allprova").style.background = bgmatt; //h9
-    break;
-    case (14), (15), (16), (17), (18):
-    document.getElementById("allprova").style.background = bgpome; //h14
-    break;
-    case (19), (20):
-    document.getElementById("allprova").style.background = bgsera; //h19
-    case (21), (22), (23):
-    document.getElementById("allprova").style.background = bgnotte; //h23
-    document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte..."; //h4
-    document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani"; //h4
-    var element = document.getElementById("animazione");
-    element.classList.add("hidden");
-    break;
+
+switch (exp) {
+  case (0):
+  document.getElementById("allprova").style.background = bgnotte; //h0
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte...";
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani";
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (1):
+  document.getElementById("allprova").style.background = bgnotte; //h0
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte...";
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani";
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (2):
+  document.getElementById("allprova").style.background = bgnotte; //h0
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte...";
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani";
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (3):
+  document.getElementById("allprova").style.background = bgnotte; //h0
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte...";
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani";
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (4):
+  document.getElementById("allprova").style.background = bgnotte; //h0
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte...";
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani";
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (5):
+  document.getElementById("allprova").style.background = bgnotte; //h0
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte...";
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani";
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (6):
+  document.getElementById("allprova").style.background = bgalba; //h6
+  break;
+  case (7):
+  document.getElementById("allprova").style.background = bgalba; //h6
+  break;
+  case (8):
+  document.getElementById("allprova").style.background = bgalba; //h6
+  break;
+  case (9):
+  document.getElementById("allprova").style.background = bgmatt; //h9
+  break;
+  case (10):
+  document.getElementById("allprova").style.background = bgmatt; //h9
+  break;
+  case (11):
+  document.getElementById("allprova").style.background = bgmatt; //h9
+  break;
+  case (12):
+  document.getElementById("allprova").style.background = bgmatt; //h9
+  break;
+  case (13):
+  document.getElementById("allprova").style.background = bgmatt; //h9
+  break;
+  case (14):
+  document.getElementById("allprova").style.background = bgpome; //h14
+  break;
+  case (15):
+  document.getElementById("allprova").style.background = bgpome; //h14
+  break;
+  case (16):
+  document.getElementById("allprova").style.background = bgpome; //h14
+  break;
+  case (17):
+  document.getElementById("allprova").style.background = bgpome; //h14
+  break;
+  case (18):
+  document.getElementById("allprova").style.background = bgpome; //h14
+  break;
+  case (19):
+  document.getElementById("allprova").style.background = bgsera; //h19
+  break;
+  case (20):
+  document.getElementById("allprova").style.background = bgsera; //h19
+  break;
+  case (21):
+  document.getElementById("allprova").style.background = bgnotte; //h23
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte..."; //h4
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani"; //h4
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (22):
+  document.getElementById("allprova").style.background = bgnotte; //h23
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte..."; //h4
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani"; //h4
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
+  case (23):
+  document.getElementById("allprova").style.background = bgnotte; //h23
+  document.getElementById("titolo").innerHTML = "<br><br><br>Buonanotte..."; //h4
+  document.getElementById("sottotitolo").innerHTML = "Ricorda di mettere la sveglia per domani"; //h4
+  var element = document.getElementById("animazione");
+  element.classList.add("hidden");
+  break;
 }
+
+
+if (exp < 10)
+{
+  expdef = ("0"+exp);
+} else {
+    expdef = exp;
+  }
+
+
+
+if (exp2 < 10)
+{
+  exp2def = ("0"+exp2);
+} else {
+    exp2def = exp2;
+  }
+
+var orario = expdef + ":" + exp2def;
+
+document.getElementById("orario").innerHTML = orario;
+
+
 /*
 document.getElementById("allprova").style.background = linear-gradient(180deg, #9FC2F8 0%, #E58C74 100%);
 */
