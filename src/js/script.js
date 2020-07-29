86305db11061e1d30c533c0205fa6b77184c2701
thebabr27 <47173624+thebabr27@users.noreply.github.com>
2020-07-29 13:53:26 +0200

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -10,32 +10,13 @@ appId: "1:1010593277055:web:1b729dc7368b8f6f"
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 console.log("firebase loaded");
-// Initialize Swiper
-var mySwiper = new Swiper (
-	'.swiper-container', {
-	direction: 'horizontal',
-	loop: true, 
-	pagination: {	
-	el: '.swiper-pagination',
-	dynamicBullets: true,
-	}, 
-	navigation: {
-	nextEl: '.swiper-button-next', 
-	prevEl: '.swiper-button-prev',
-	}
-});
-console.log("swiper loaded");const buttonNewRes = document.getElementById("buttonNewRes");const buttonEditRes = document.getElementById("buttonEditRes");const inputName = document.getElementById("inputName");const buttonConfirm = document.getElementById("buttonConfirm");const buttonDaySelected = document.getElementById("buttonDaySelected");const codeForm = document.getElementById("codeForm");var inputDay ="", inputMonth ="", inputYear ="";var inputService = document.getElementById("inputService");var nameValidation=false,surnameValidation=false,emailValidation=false,phoneValidation=false,serviceValidation=false,dayValidation=false,monthValidation=false,yearValidation=false,previousSelDay;function toggleGoEditRes() { 
-verifyValidation(1);
-setTimeout(( ) => {
-	document.getElementById("inputName").focus();
-
-}, 500);
-
+var swiperH = new Swiper('.swiper-container-h', {cssMode:true,spaceBetween: 50,pagination: {el: '.swiper-pagination-h',clickable: true,},});var swiperV = new Swiper('.swiper-container-v', {cssMode:true,direction: 'vertical',spaceBetween: 50,speed: 600,pagination: {el: '.swiper-pagination-v',clickable: true,},});const buttonNewRes = document.getElementById("buttonNewRes");const buttonEditRes = document.getElementById("buttonEditRes");const buttonGoEditRes = document.getElementById("buttonGoEditRes");const inputName = document.getElementById("inputName");const buttonConfirm = document.getElementById("buttonConfirm");const buttonDaySelected = document.getElementById("buttonDaySelected");const codeForm = document.getElementById("codeForm");var inputDay ="", inputMonth ="", inputYear ="";var inputService = document.getElementById("inputService");var nameValidation=false,surnameValidation=false,emailValidation=false,phoneValidation=false,serviceValidation=false,dayValidation=false,monthValidation=false,yearValidation=false,previousSelDay;function toggleNewRes() { 
+bulletClick(14);
 
 };
-buttonGoEditRes.addEventListener('click', e => {
+buttonNewRes.addEventListener('click', e => {
 
-toggleGoEditRes();
+toggleNewRes();
 });
 
 function toggleGoData() { 
@@ -65,41 +46,17 @@ buttonHourSelected.addEventListener('click', e => {
 toggleGoHour();
 });
 
-function verifyValidation(section) { 
-switch (section) {
-case 1:
-if(buttonEditRes.classList.contains("focus")){
-	
-} else {
-	location.href = "#section1";
+function toggleConfirmRes() { 
+verifyValidation(5);
 
-}
-break;
-case 2:
-nameValidation= validateInput(inputName);
-surnameValidation= validateInput(inputSurname);
-emailValidation= validateInput(inputEmail);
-phoneValidation= validateInput(inputPhone);
-if(nameValidation && surnameValidation && emailValidation && phoneValidation){
-	location.href = "#section2";
+};
+buttonConfirm.addEventListener('click', e => {
 
-}
-break;
-case 3:
-console.log(inputDay+" "+dayValidation);
+toggleConfirmRes();
+});
 
-console.log(inputYear+" "+yearValidation);
-
-console.log(inputMonth+" "+monthValidation);
-
-location.href = "#section3";
-break;
-case 4:
-location.href = "#section4";
-	document.getElementById("riepilogo").innerHTML = "<h5 class=' card-title text-center'>Riepilogo Prenotazione</h5><p class=' card-text'>"+inputName.value+"</p><p class=' card-text'>"+inputSurname.value+"</p><p class=' card-text'>"+inputDay+"/"+inputMonth+"/"+inputYear+"</p><p class=' card-text'>"+inputHour.value+":"+inputMinutes.value+"</p>";;
-
-break;
-}
+function bulletClick(bul) { 
+document.getElementsByClassName("swiper-pagination-bullet")[bul].click();
 
 };
 function validateInput(theInput) { 
@@ -114,30 +71,6 @@ theInput.setAttribute("valid","true");
 return true
 
 }
-
-};
-function toggleNewRes() { 
-toggleNewEditRes();
-toggleCodeForm();
-
-};
-buttonNewRes.addEventListener('click', e => {
-
-toggleNewRes();
-});
-
-function toggleEditRes() { 
-toggleNewEditRes();
-toggleCodeForm();
-
-};
-buttonEditRes.addEventListener('click', e => {
-
-toggleEditRes();
-});
-
-function toggleCodeForm() { 
-codeForm.classList.toggle("d-none");
 
 };
 function calendarClick(day) { 
@@ -159,19 +92,67 @@ previousSelDay = day;
 buttonDaySelected.classList.remove("disabled");
 
 };
-function toggleNewEditRes() { 
-buttonNewRes.classList.toggle("active");
-buttonNewRes.classList.toggle("focus");
-buttonEditRes.classList.toggle("active");
-buttonEditRes.classList.toggle("focus");
+function verifyValidation(section) { 
+switch (section) {
+case 1:
+if(buttonEditRes.classList.contains("focus")){
+	
+} else {
+	location.href = "#section1";
+
+}
+break;
+case 2:
+nameValidation= validateInput(inputName);
+surnameValidation= validateInput(inputSurname);
+emailValidation= validateInput(inputEmail);
+phoneValidation= validateInput(inputPhone);
+if(nameValidation && surnameValidation && emailValidation && phoneValidation){
+	document.getElementById("dataSpin").classList.toggle("d-none");
+setTimeout(( ) => {
+	document.getElementById("dataSpin").classList.toggle("d-none");
+bulletClick(15);
+
+}, 500);
+
+
+}
+break;
+case 3:
+console.log(inputDay+" "+dayValidation);
+
+console.log(inputYear+" "+yearValidation);
+
+console.log(inputMonth+" "+monthValidation);
+
+document.getElementById("calendarSpin").classList.toggle("d-none");
+setTimeout(( ) => {
+	document.getElementById("calendarSpin").classList.toggle("d-none");
+bulletClick(16);
+
+}, 500);
+
+break;
+case 4:
+document.getElementById("hourSpin").classList.toggle("d-none");
+setTimeout(( ) => {
+	document.getElementById("hourSpin").classList.toggle("d-none");
+bulletClick(17);
+
+}, 500);
+
+	document.getElementById("riepilogo").innerHTML = "<h5 class=' mb-4 card-title text-center'>Riepilogo Prenotazione</h5><p class=' card-text text-left'>Nome: "+inputName.value+"</p><p class=' card-text text-left'>Cognome: "+inputSurname.value+"</p><p class=' card-text text-left'>Data appuntamento: "+inputDay+"/"+inputMonth+"/"+inputYear+"</p><p class=' card-text text-left'>Orario appuntamento: "+inputHour.value+":"+inputMinutes.value+"</p><p class=' card-text text-left'>Servizio richiesto: "+inputService.value+"</p>";;
+
+break;
+case 5:
+document.getElementById("confirmSpin").classList.toggle("d-none");
+setTimeout(( ) => {
+	document.getElementById("confirmSpin").classList.toggle("d-none");
+firebase.database().ref("db/aiello/public/"+inputSurname.value+"_"+inputName.value).set({"service": inputService.value,"name": inputName.value,"surname": inputSurname.value,"phone": inputPhone.value,"email": inputEmail.value,"hour": inputHour.value,"minutes": inputMinutes.value,"day": inputDay,"month": inputMonth,"year": inputYear,});
+
+}, 500);
+
+break;
+}
 
 };
-function confirmAction() { 
-firebase.database().ref("db/aiello/public/customer1").set({"service": inputService.value,"name": inputName.value,"surname": inputSurname.value,"phone": inputPhone.value,"email": inputEmail.value,"hour": inputHour.value,"minutes": inputMinutes.value,"day": inputDay,"month": inputMonth,"year": inputYear,});
-
-};
-buttonConfirm.addEventListener('click', e => {
-
-confirmAction();
-});
-
